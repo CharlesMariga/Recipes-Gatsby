d2569a98e97db8daf771692e38b20a37ac3af70d
charlesmariga <charlesmariga37@gmail.com>
2022-02-19 23:19:58 +0300

    setup recipeslist

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -3,19 +3,18 @@ import { Link } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 export default function RecipesList({ recipes = [] }) {
-  console.log(recipes);
-
   return (
     <div className="recipes-list">
       {recipes.map(recipe => (
         <Link key={recipe.id} to={`/${recipe.title}`} className="recipe">
           <GatsbyImage
-            image={getImage(recipe.image.gatsbyImageData)}
-            class="recipe-img"
+            image={getImage(recipe.image)}
+            className="recipe-img"
+            alt={recipe.title}
           />
           <h5>{recipe.title}</h5>
           <p>
-            Prep: {recipe.prepTime} min | Cook: {recipe.cookTime} mins
+            Prep: {recipe.prepTime} min | Cook: {recipe.cookTime} min
           </p>
         </Link>
       ))}
