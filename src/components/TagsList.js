23b17453d07c11c5153badfba02eb680ec9cdd97
charlesmariga <charlesmariga37@gmail.com>
2022-02-21 07:49:32 +0300

    Setup tags

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,9 +1,20 @@
 import React from "react";
+import setupRecipes from "../utils/setupTags";
+import { graphql, Link } from "gatsby";
 
-export default function TagsList() {
+export default function TagsList({ recipes = [] }) {
+  const newTags = setupRecipes(recipes);
+
   return (
-    <div>
-      <h4>Tags list</h4>
+    <div className="tag-container">
+      <h4>Recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => (
+          <Link to={`/${tag[0]}`} key={index}>
+            {tag[0]} ({tag[1]})
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/utils/setupTags.js
@@ -0,0 +1,20 @@
+const setupRecipes = recipes => {
+  const allTags = {};
+
+  recipes.forEach(recipe =>
+    recipe.content.tags.forEach(tag => {
+      if (allTags[tag]) {
+        allTags[tag]++;
+      } else {
+        allTags[tag] = 1;
+      }
+    })
+  );
+
+  return Object.entries(allTags).sort((a, b) => {
+    const [firstTag] = a;
+    const [secondTag] = b;
+    return firstTag.localeCompare(secondTag);
+  });
+};
+export default setupRecipes;
