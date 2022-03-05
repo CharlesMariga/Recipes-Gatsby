2fe87ee18f4628ec24b2a891a988c456cf21dad3
charlesmariga <charlesmariga37@gmail.com>
2022-03-05 23:45:23 +0300

    Started setting up the recipes page

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,13 +1,58 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { BsClockHistory, BsClock, BsPeople } from "react-icons/bs";
+import Layout from "../components/Layout";
 
-export default function RecipeTemplate(props) {
-  console.log(props);
+export default function RecipeTemplate({ data }) {
+  const {
+    title,
+    cookTime,
+    content,
+    description: { description },
+    servings,
+    prepTime,
+    image,
+  } = data.contentfulRecipe;
+
+  const { ingredeints, instructions, tags, tools } = content;
+  const pathToImage = getImage(image);
 
   return (
-    <div>
-      <h2>{props.params.title}</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <div className="recipe-page">{/* hero */}</div>
+        <section className="recipe-hero">
+          <GatsbyImage image={pathToImage} alt={title} class="about-img" />
+          <article className="recipe-info">
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <div className="recipe-icons">
+              <article>
+                <BsClock />
+                <h5>prep time</h5>
+                <p>{prepTime} min.</p>
+              </article>
+              <article>
+                <BsPeople />
+                <h5>servings</h5>
+                <p>{servings}.</p>
+              </article>
+            </div>
+            <p className="recipe-tags">
+              Tags :{" "}
+              {tags.map((tag, index) => (
+                <Link to={`/${tag}`} key={index}>
+                  {tag}
+                </Link>
+              ))}
+            </p>
+          </article>
+        </section>
+        {/* rest of the content */}
+        <section className="recipe-content"></section>
+      </main>
+    </Layout>
   );
 }
 
@@ -26,6 +71,9 @@ export const query = graphql`
         instructions
         tags
         tools
+      }
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
       }
     }
   }
