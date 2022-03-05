e41752398f55ea139b0a28524724f44a89f39d58
charlesmariga <charlesmariga37@gmail.com>
2022-03-05 23:11:49 +0300

    Setup page query

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { graphql } from "gatsby";
 
-export default function RecipeTemplate({ params }) {
+export default function RecipeTemplate(props) {
+  console.log(props);
+
   return (
     <div>
-      <h2>{params.title}</h2>
+      <h2>{props.params.title}</h2>
     </div>
   );
 }
+
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      title
+      cookTime
+      description {
+        description
+      }
+      servings
+      prepTime
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+    }
+  }
+`;
